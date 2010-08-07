dd6b8f159b8c768839b730a7a04af63a48480aa7
rainer.hessmer <rainer.hessmer@38d7aa61-b10b-63a5-20ab-7551e83da41b>
2010-08-07 03:45:53 +0000

    TanslationAnalyzer removes rotation effect and filters out translation vectors that are too far off the direction that the robot is pointing at.

--- VisualOdometry/TranslationAnalyzer.cs
+++ VisualOdometry/TranslationAnalyzer.cs
@@ -32,10 +32,13 @@ namespace VisualOdometry
 			double s = Math.Sin(headingChange.Rads);
 			double c = Math.Cos(headingChange.Rads);
 
+			double acceptedDirectionMisaligment = 10 * Math.PI / 180;
+
 			m_TranslationIncrements.Clear();
 			System.Drawing.PointF[] featurePointPair = new System.Drawing.PointF[2];
 
 			double sumX = 0, sumY = 0;
+			int groundFeatureCount = 0;
 
 			List<TrackedFeature> trackedFeatures = m_VisualOdometer.TrackedFeatures;
 			for (int i = 0; i < trackedFeatures.Count; i++)
@@ -52,6 +55,8 @@ namespace VisualOdometry
 					continue;
 				}
 
+				groundFeatureCount++;
+
 				featurePointPair[0] = trackedFeature[-1]; // previous feature location
 				featurePointPair[1] = trackedFeature[0];  // current featue location
 				//Debug.WriteLine("Raw:");
@@ -65,17 +70,24 @@ namespace VisualOdometry
 
 	
 				// Remove rotation effect on current feature location. The center of the rotation is the previous feature location
-				//Point rotationCorrectedEndPoint = new Point(
-				//    c * featurePointPair[1].X - s * featurePointPair[1].Y,
-				//    s * featurePointPair[1].X + c * featurePointPair[1].Y);
-
 				Point rotationCorrectedEndPoint = new Point(
-					featurePointPair[1].X,
-					featurePointPair[1].Y);
+					c * featurePointPair[1].X - s * featurePointPair[1].Y,
+					s * featurePointPair[1].X + c * featurePointPair[1].Y);
+
+				//Point rotationCorrectedEndPoint = new Point(
+				//    featurePointPair[1].X,
+				//    featurePointPair[1].Y);
 
 				Point translationIncrement = new Point(
 					featurePointPair[0].X - rotationCorrectedEndPoint.X,
 					featurePointPair[0].Y - rotationCorrectedEndPoint.Y);
+
+				double translationAngle = Math.Abs(Math.Atan2(translationIncrement.X, translationIncrement.Y));
+				//Debug.WriteLine(translationAngle * 180 / Math.PI);
+				if (translationAngle > acceptedDirectionMisaligment)
+				{
+					continue;
+				}
 
 				//Debug.WriteLine("Delta: dx_r: {0:0.000}  dy_r: {1:0.000}", translationIncrement.X, translationIncrement.Y);
 
@@ -83,6 +95,8 @@ namespace VisualOdometry
 				sumX += translationIncrement.X;
 				sumY += translationIncrement.Y;
 			}
+
+			Debug.WriteLine("Used ground features %: " + ((double)m_TranslationIncrements.Count/(double)groundFeatureCount).ToString());
 
 			if (m_TranslationIncrements.Count > 0)
 			{
